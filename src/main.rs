c37fe51298d69535b693020ccce977f29511ebb7
joshhartigan <joshhartigan99@gmail.com>
2014-12-26 11:21:24 +0000

    capability to read file, and print line by line

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use std::os;
+use std::os; // for os::args
+use std::io::File;
 
 fn main() {
     let args = os::args();
-    if args.len() > 1 {
-        println!( "the first argument is {}", args[1] );
-    } else {
-        println!("you didn't give any arguments");
+    if args.len() <= 1 {
+        panic!("you must specify a file to process");
     }
+
+    let filename = &args[1].as_slice();
+    let path = Path::new(filename);
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(error) => panic!("failed to open {}: {}", display, error.desc),
+        Ok(file) => file,
+    };
+
+    match file.read_to_string() {
+        Err(error) => panic!("failed to read {}: {}", display, error.desc),
+        Ok(string) => print!("{}", string),
+    };
+
+    // the file is closed and `file` is taken out of scope
 }
 
